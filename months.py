c38182e2e870438707ea706e57d015697162f941
mindanielk <61715929+mindanielk@users.noreply.github.com>
2025-01-29 22:46:39 +0000

    Added an enum for months and created a template for adding a calendar

---
+++ months.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Months(Enum):
+    January = ["January", 31]
+    February = ["February", 28]
+    March = ["March", 31]
+    April = ["April", 30]
+    May = ["May", 31]
+    June = ["June", 30]
+    July = ["July", 31]
+    August = ["August", 31]
+    September = ["September", 30]
+    October = ["October", 31]
+    November = ["November", 30]
+    December = ["December", 31]
